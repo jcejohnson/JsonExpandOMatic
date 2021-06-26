d7e89beb75e6fdd7581c4ff70d7e7a8b1db08ebd
James Johnson <jcejohnson@gmail.com>
2021-06-26 14:16:17 -0400

    Don't automatically update requirements.txt in wrapper.sh.

--- wrapper.sh
+++ wrapper.sh
@@ -11,19 +11,18 @@ if [ ! -d venv ] ; then
     venv/bin/pip install --upgrade pip
     venv/bin/pip install pip-tools
   )
-  # Force an update of *requirements.txt for _this_ version of python
-  # (for instance, 3.6 will want dataclasses==0.8 but that fails for 3.8)
-  touch requirements.in dev-requirements.in
+  # Force the `if` below to _not_ recomplie requirements*txt so that we
+  #   use what the develop has explicitly requested in them.
+  # Recompiling them is handy sometimes but a bit obnoxious for a 
+  #   reusable library.
+  touch requirements.txt dev-requirements.in
 fi
 
-if [ requirements.in -nt requirements.txt ] || [ dev-requirements.in -nt dev-requirements.txt ]
-then
-  (
-    set -x
-    venv/bin/pip-compile requirements.in
-    venv/bin/pip-compile dev-requirements.in
-  )
-fi
+(
+  set -x
+  [ requirements.in -nt requirements.txt ] && venv/bin/pip-compile requirements.in
+  [ dev-requirements.in -nt dev-requirements.txt ] && venv/bin/pip-compile dev-requirements.in
+)
 
 [ -x venv/bin/JsonExpandOMatic ] || (set -x ; venv/bin/pip install -e .)
 
