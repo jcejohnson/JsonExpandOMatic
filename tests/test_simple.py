60d11cc625bfd86dea1d197d7ca2e71e58fc83b9
James Johnson <jcejohnson@gmail.com>
2022-11-17 22:18:10 -0500

    Cleanup the tests: - Dump unittest & commit to pytest. - Extract raw data into a resource file. - Break 'test all things' into bite-sized pieces.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,115 +1,82 @@
 import json
-import shutil
-import tempfile
-import unittest
 
 import jsonref  # type: ignore
+import pytest
 
 from json_expand_o_matic import JsonExpandOMatic
 
-raw_data = {
-    "actors": {
-        "charlie_chaplin": {
-            "first_name": "Charlie",
-            "last_name": "Chaplin",
-            "is_funny": True,
-            "birth_year": 1889,
-            "filmography": [
-                ("The Kid", 1921),
-                ("A Woman of Paris", 1923),
-                # ...
-                ("Modern Times", 1936),
-            ],
-            "movies": {
-                "modern_times": {
-                    "title": "Modern Times",
-                    "year": 1936,
-                    "budget": 1500000,
-                    "run_time_minutes": 87,
-                }
-            },
-        },
-        "dwayne_johnson": {
-            "first_name": "Dwayne",
-            "last_name": "Johnson",
-            "hobbies": {},
-            "movies": [
-                {
-                    "title": "Fast Five",
-                    "cast": {
-                        "dominic_toretto": {
-                            "name": "Dominic Toretto",
-                            "actor": "vin_diesel",
-                        },
-                        "brian_oconner": {
-                            "name": "Brian O'Conner",
-                            "actor": "paul_walker",
-                        },
-                        "mia_toretto": {
-                            "name": "Mia Toretto",
-                            "actor": "jordana_brewster",
-                        },
-                    },
-                }
-            ],
-        },
-    }
-}
 
+class TestSimple:
 
-data_path = tempfile.mkdtemp()  # /tmp/tmp8d6hy1iq
-data_path_basename = data_path.split("/")[-1]
-data_path_dirname = "/".join(data_path.split("/")[0:-1])
+    # Our raw test data.
+    _raw_data = None
 
+    @pytest.fixture
+    def raw_data(self, resource_path_root):
+        if not TestSimple._raw_data:
+            TestSimple._raw_data = json.loads((resource_path_root / "actor-data.json").read_text())
+        return TestSimple._raw_data
 
-class TestSimple(unittest.TestCase):
-    def test_all_things(self):
-        try:
-            return self._test_all_things()
-        finally:
-            shutil.rmtree(data_path)
+    # Fixtures to provide copies of the raw data to each test function.
 
-    def _test_all_things(self):
+    @pytest.fixture
+    def test_data(self, raw_data):
+        return json.loads(json.dumps(raw_data))
 
-        # Make two copies of our raw data.
-        # One will be our test victim, the other will be for validation.
-        test_data = json.loads(json.dumps(raw_data))
-        original_data = json.loads(json.dumps(raw_data))
+    @pytest.fixture
+    def original_data(self, raw_data):
+        return json.loads(json.dumps(raw_data))
+
+    def test_equivalency(self, test_data, original_data):
+        # Assert that independent copies of the raw data are equivalent.
         assert test_data == original_data
 
-        expanded = JsonExpandOMatic(path=data_path).expand(test_data, root_element="root", preserve=True)
+    def test_expand_preserve(self, tmpdir, test_data, original_data):
+        expanded = JsonExpandOMatic(path=tmpdir).expand(test_data, root_element="root", preserve=True)
+
+        # preserve=True prevents mangling of test_data by expand()
         assert test_data == original_data
+
         # expand() returns a new representation of `data`
-        assert expanded == {"root": {"$ref": f"{data_path_basename}/root.json"}}
-        #                  {'root': {'$ref': 'tmp8d6hy1iq/root.json'}}
+        assert expanded == {"root": {"$ref": f"{tmpdir.basename}/root.json"}}
 
-        expanded = JsonExpandOMatic(path=data_path).expand(test_data, root_element="root", preserve=False)
+    def test_expand_mangle(self, tmpdir, test_data, original_data):
+        expanded = JsonExpandOMatic(path=tmpdir).expand(test_data, root_element="root", preserve=False)
+
+        # preserve=True allows mangling of test_data by expand()
         assert test_data != original_data
+
+        # test_data is the content of "{tmpdir.basename}/root.json"
+        assert test_data == {"actors": {"$ref": "root/actors.json"}}
+        assert test_data == json.loads(tmpdir.join("root.json").read())
+
         # expand() returns a new representation of `data`
-        assert expanded == {"root": {"$ref": f"{data_path_basename}/root.json"}}
-        #                  {'root': {'$ref': 'tmp8d6hy1iq/root.json'}}
+        assert expanded == {"root": {"$ref": f"{tmpdir.basename}/root.json"}}
+
+    def test_contract(self, tmpdir, test_data, original_data):
+        expanded = JsonExpandOMatic(path=tmpdir).expand(test_data, root_element="root", preserve=False)
+        assert expanded == {"root": {"$ref": f"{tmpdir.basename}/root.json"}}
+
+        # We can use JsonExpandOMatic() to load the expanded data from the filesystem.
+        # Note that this returns the original data exactly, the `root` wrapper is removed.
+        contracted = JsonExpandOMatic(path=tmpdir).contract(root_element="root")
+        assert contracted == original_data
+
+        # Or we can use jsonref.load() to do the same.
+        with open(f"{tmpdir}/root.json") as f:
+            assert jsonref.load(f, base_uri=f"file://{tmpdir}/") == original_data
+
+    def test_jsonref(self, tmpdir, test_data, original_data):
+        expanded = JsonExpandOMatic(path=tmpdir).expand(test_data, root_element="root", preserve=False)
 
         # We can use jsonref to load this new representation.
         # Note that loading in this way exposes the wrapping element `root`.
-        # `data_path` must be a fully qualified path.
-        loaded = jsonref.loads(json.dumps(expanded), base_uri=f"file://{data_path_dirname}/")
+        # `tmpdir` must be a fully qualified path.
+        loaded = jsonref.loads(json.dumps(expanded), base_uri=f"file://{tmpdir.dirname}/")
         assert loaded == {"root": original_data}
         assert loaded["root"] == original_data
 
         # A raw load of the wrapping document has references to the sub-elements.
         # This assersion assumes that the original data's elements are all dicts.
-        with open(f"{data_path}/root.json") as f:
+        with open(f"{tmpdir}/root.json") as f:
             assert json.load(f) == {k: {"$ref": f"root/{k}.json"} for k, v in original_data.items()}
-
-        # We can use JsonExpandOMatic() to load the expanded data from the filesystem.
-        # Note that this returns the original data exactly, the `root` wrapper is removed.
-        contracted = JsonExpandOMatic(path=data_path).contract(root_element="root")
-        assert contracted == original_data
-
-        # Or we can use jsonref.load() to do the same.
-        with open(f"{data_path}/root.json") as f:
-            assert jsonref.load(f, base_uri=f"file://{data_path}/") == original_data
-
-
-if __name__ == "__main__":
-    unittest.main()
