b8ad01181006b64ada1770f257099b3f264d44b9
James Johnson <jcejohnson@gmail.com>
2023-06-03 11:28:54 -0400

    Functional lazy loading

--- tests/test_pydantic.py
+++ tests/test_pydantic.py
@@ -1,11 +1,13 @@
+import abc
 import copy
 import json
-from typing import ClassVar, Dict, List, Optional, Type, Union
+from typing import Any, Dict, List, Optional, Union
 
 import pytest
 
-from pydantic import BaseModel, Field, PrivateAttr
-from pydantic.main import create_model
+from json_expand_o_matic.expand_o_matic import JsonExpandOMatic
+from pydantic import BaseModel, Field
+from pydantic.class_validators import root_validator
 
 DATA = {
     "actors": {
@@ -19,48 +21,73 @@ DATA = {
 }
 
 
-class LazyLoaderBase(BaseModel):
-    """ """
-
-    ref_: str = Field(..., alias="$ref")
-    data_: Optional[BaseModel] = None
-
-    def __getattr__(self, name):
-        if not self.data_:
-            self.data_ = self.type_(**DATA["actors"][self.ref_])
-        return getattr(self.data_, name)
-
-
 class LazyMaker:
     """ """
 
+    class LazyProxyBase(BaseModel):
+        pass
+
+    class LazyLoaderBase(abc.ABC):
+        @abc.abstractmethod
+        def load(self, name: str, data_type: type, reference: str) -> Any:
+            ...
+
     ACTUAL_MODELS = {}
+    ALL_MODELS = {}
     LAZY_MODELS = {}
 
+    def __init__(self, loader: LazyLoaderBase, ref_key: str = "$ref") -> None:
+        self._loader = loader
+        self._ref_key = ref_key
+
     def __getitem__(self, class_name: str) -> type:
-        v = self.ACTUAL_MODELS.get(class_name) or self.LAZY_MODELS.get(class_name)
-        if v:
-            return v
-        if class_name.endswith("LazyModel") or class_name.endswith("ActualModel"):
+        v = self.ALL_MODELS.get(class_name)
+        if v or class_name.endswith("LazyModel") or class_name.endswith("ActualModel"):
             return v
         return self.ACTUAL_MODELS.get(f"{class_name}ActualModel")
 
     def __call__(self, clazz):
         """ """
 
-        class LazyLoader(LazyLoaderBase):
-            type_: type = clazz
+        data_loader = self._loader
+        ref_key = self._ref_key
 
-        # LazyModel = create_model(f"{clazz.__name__}LazyModel", __base__=LazyLoader, __module__=__name__)
-        # ActualModel = create_model(f"{clazz.__name__}ActualModel", __base__=clazz, __module__=__name__)
+        class LazyProxy(LazyMaker.LazyProxyBase):
+            class Config:
+                underscore_attrs_are_private: bool = True
 
-        self.LAZY_MODELS[f"{clazz.__name__}LazyModel"] = LazyLoader
-        self.ACTUAL_MODELS[f"{clazz.__name__}ActualModel"] = clazz
+            ref_: str  # PrivateAttr does not support alias and this isn't really a private attribute anyway.
+            _data: Optional[clazz] = None  # The actual model after it has been lazily loaded.
+            _type: type = clazz  # The datatype of the actual model.
 
-        return Union[LazyLoader, clazz]
+            @root_validator(pre=True, allow_reuse=True)
+            @classmethod
+            def lazy_proxy_root_validator(cls, values):
+                if ref_key in values:
+                    values["ref_"] = values.pop(ref_key)
+                return values
+
+            def __getattr__(self, name):
+                """
+                Lazily load the actual model on first reference and delegate all attribute requests to it.
+                """
+                if not self._data:
+                    self._data = data_loader.load(name=name, data_type=self._type, reference=self.ref_)
+                return getattr(self._data, name)
+
+        self.ALL_MODELS[f"{clazz.__name__}LazyModel"] = self.LAZY_MODELS[f"{clazz.__name__}LazyModel"] = LazyProxy
+        self.ALL_MODELS[f"{clazz.__name__}ActualModel"] = self.ACTUAL_MODELS[f"{clazz.__name__}ActualModel"] = clazz
+
+        clazz.LAZY_UNION = Union[LazyProxy, clazz]
+        return clazz
 
 
-make_lazy = LazyMaker()
+class AktorDataLazyLoader(LazyMaker.LazyLoaderBase):
+    def load(self, name: str, data_type: type, reference: str) -> Any:
+        return data_type(**DATA["actors"][reference])
+
+
+make_lazy = LazyMaker(loader=AktorDataLazyLoader())
 
 
 @make_lazy
@@ -73,11 +100,11 @@ class Movie(BaseModel):
 class Actor(BaseModel):
     first_name: str
     last_name: str
-    movies: Optional[Union[Dict[str, Movie], List[Movie]]] = Field(default=None)
+    movies: Optional[Union[Dict[str, Movie.LAZY_UNION], List[Movie.LAZY_UNION]]] = Field(default=None)
 
 
 class Model(BaseModel):
-    actors: Dict[str, Actor]
+    actors: Dict[str, Actor.LAZY_UNION]
 
 
 class TestPydantic:
@@ -88,17 +115,25 @@ class TestPydantic:
         return json.loads((resource_path_root / "actor-data.json").read_text())
 
     @pytest.fixture
+    def test_data(self, raw_data):
+        return json.loads(json.dumps(raw_data))
+
+    @pytest.fixture
+    def expanded(self, tmpdir, test_data):
+        return JsonExpandOMatic(path=tmpdir).expand(test_data, root_element="root")
+
+    @pytest.fixture
     def data(self):
         return copy.deepcopy(DATA)
 
-    def test_one(self, data):
+    def test_traditional(self, data):
         m = Model.parse_obj(data)
         assert not isinstance(m.actors["dwayne_johnson"], make_lazy["ActorLazyModel"])
         assert isinstance(m.actors["dwayne_johnson"], make_lazy["ActorActualModel"])
         assert isinstance(m.actors["dwayne_johnson"], make_lazy["Actor"])
         assert m.actors["dwayne_johnson"].first_name == "Dwayne"
 
-    def test_two(self, data):
+    def test_lazy(self, data):
         data["actors"]["dwayne_johnson"] = {"$ref": "dwayne_johnson"}
 
         Model.Config.orm_mode = True
@@ -108,12 +143,15 @@ class TestPydantic:
         assert not isinstance(m.actors["dwayne_johnson"], make_lazy["Actor"])
         assert m.actors["dwayne_johnson"].first_name == "Dwayne"
 
-    def test_three(self):
-        assert make_lazy["ActorLazyModel"].type_ is make_lazy["Actor"]
-        assert make_lazy["MovieLazyModel"].type_ is make_lazy["Movie"]
+    def test_get_model(self):
+        assert make_lazy["ActorLazyModel"]._type is make_lazy["Actor"]
+        assert make_lazy["MovieLazyModel"]._type is make_lazy["Movie"]
 
-    def test_four(self, data):
+    def test_attribute_update(self, data):
         Model.Config.orm_mode = True
         m = Model.parse_obj(data)
         m.actors["dwayne_johnson"].first_name = "Mr."
         assert m.actors["dwayne_johnson"].first_name == "Mr."
+
+    def test_expanded_laziness(self, expanded):
+        ...
